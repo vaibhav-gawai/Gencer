2de335e5f9436bd06169f118c158986dabeb64f4
Vaibhav Gawai <vaibhavgawai@protonmain.com>
2021-08-18 18:16:50 +0530

    Small Changes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 import random
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A1, A4, letter
+from reportlab.lib.pagesizes import letter
 pdfmetrics.registerFont(TTFont('poppins', 'Fonts\Poppins-Bold.ttf'))
 
 
